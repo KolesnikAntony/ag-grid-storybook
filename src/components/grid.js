b32278afb8da014dcf67aa143870f87d5d22a271
Anton <kolesiaaa@gmail.com>
2022-04-05 15:55:42 +0300

    for stats loading

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,63 +1,122 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import { AgGridColumn, AgGridReact } from 'ag-grid-react';
-import ButtonView from '../buttons/button-view';
-import ButtonPrint from '../buttons/button-print';
-import SendIconComponent from './SendIcon';
-import ButtonDispatch from '../buttons/button-dispatсh';
-import PopupInvoice from '../popups/popup-invoice';
+import React, { useCallback, useMemo, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-enterprise';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import Loading from './loading';
+import PropTypes from 'prop-types';
 
-const Grid = ({ filteredData, column, setGridApi, onSelectionChanged }) => {
-  const onGridReady = (params) => {
-    setGridApi(params.api);
-  };
+const Grid = (props) => {
+  const { pagination, rowCount, isLoading, isError, isAuth, isEmpty } = props;
 
-  const handleSendType = (params) => {
-    console.log(params);
+  const containerStyle = useMemo(() => ({ width: '100%', height: '800px' }), []);
+  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+
+  const [columnDefs] = useState([
+    { field: 'id' },
+    { field: 'athlete', width: 150 },
+    { field: 'age' },
+    { field: 'country' },
+    { field: 'year' },
+    { field: 'sport' },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+  ]);
+  const defaultColDef = useMemo(() => {
+    return {
+      editable: true,
+      sortable: true,
+      flex: 1,
+      minWidth: 100,
+      filter: true,
+      resizable: true,
+    };
+  }, []);
+
+  const loadingCellRenderer = useMemo(() => {
+    return Loading;
+  }, []);
+
+  const loadingCellRendererParams = useMemo(() => {
+    return {
+      loadingMessage: 'One moment please...',
+    };
+  }, []);
+
+  const onGridReady = useCallback((params) => {
+    // fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    //   .then((resp) => resp.json())
+    //   .then((data) => {
+    //     // add id to data
+    //     let idSequence = 0;
+    //     data.forEach((item) => {
+    //       item.id = idSequence++;
+    //     });
+    //     const server = getFakeServer(data);
+    //     const datasource = getServerSideDatasource(server);
+    //     params.api.setServerSideDatasource(datasource);
+    //   });
+  }, []);
+
+  const loadingCellRendererSelector = (params) => {
+    console.log(params, '---selector');
+    const useCustomRenderer = true;
+    if (useCustomRenderer) {
+      return {
+        component: Loading,
+        params: {
+          // parameters to supply to the custom loading cell renderer
+          loadingMessage: '--- CUSTOM ERROR MESSAGE ---',
+        },
+      };
+    } else {
+      // no loading cell renderer
+      return undefined;
+    }
   };
 
   return (
-    <Box className="ag-theme-alpine" sx={{ height: '600px', width: 'calc(100% - 32px)', p: 2 }}>
-      <AgGridReact
-        frameworkComponents={{
-          viewPop: PopupInvoice,
-          getData: ButtonPrint,
-          sendIcon: SendIconComponent,
-          dispatch: ButtonDispatch,
-        }}
-        components={{
-          sendType: handleSendType,
-        }}
-        rowClass="rowClass"
-        rowData={filteredData}
-        fullWidthCellRenderer={'fullWidthCellRenderer'}
-        rowSelection={'multiple'}
-        suppressRowClickSelection={true}
-        onSelectionChanged={onSelectionChanged}
-        onGridReady={onGridReady}>
-        {!!filteredData.length &&
-          Object.keys(filteredData[0]).map(
-            (key) =>
-              key !== ('id' && 'dispatch') && (
-                <AgGridColumn
-                  hide={!column[key]}
-                  sortable={key === 'status'}
-                  filter={key === 'total' ? 'agNumberColumnFilter' : null}
-                  key={key}
-                  field={key}
-                  flex={1}
-                  resizable={true}
-                />
-              )
-          )}
-        <AgGridColumn headerName={'Dispatch'} width={100} suppressMenu={true} cellRenderer="sendIcon" />
-        <AgGridColumn width={60} cellStyle={{ textAlign: 'center' }} suppressMenu={true} cellRenderer="viewPop" />
-        <AgGridColumn width={60} cellStyle={{ textAlign: 'center' }} suppressMenu={true} cellRenderer="dispatch" />
-        <AgGridColumn width={60} cellStyle={{ textAlign: 'center' }} suppressMenu={true} cellRenderer="getData" />
-        <AgGridColumn width={40} suppressMenu={true} multiple checkboxSelection={true} />
-      </AgGridReact>
-    </Box>
+    <div style={containerStyle}>
+      <div style={{ height: '100%', paddingTop: '25px', boxSizing: 'border-box' }}>
+        <div style={gridStyle} className="ag-theme-alpine">
+          <AgGridReact
+            columnDefs={columnDefs}
+            defaultColDef={defaultColDef}
+            loadingCellRenderer={loadingCellRenderer}
+            loadingCellRendererParams={loadingCellRendererParams}
+            // loadingCellRendererSelector={loadingCellRendererSelector}
+            // suppressLoadingOverlay={true}
+            // overlayLoadingTemplate={<Loading/>}
+            cacheBlockSize={100}
+            maxBlocksInCache={10}
+            animateRows={true}
+            onGridReady={onGridReady}
+            pagination={pagination}
+            paginationPageSize={rowCount}
+          />
+        </div>
+      </div>
+    </div>
   );
+};
+
+Grid.propTypes = {
+  pagination: PropTypes.bool,
+  rowCount: PropTypes.number,
+  isLoading: PropTypes.bool,
+  isError: PropTypes.oneOfType([PropTypes.string, PropTypes.oneOf([null])]),
+  isAuth: PropTypes.bool,
+  isEmpty: PropTypes.bool,
+};
+
+Grid.defaultProps = {
+  pagination: true,
+  rowCount: 10,
+  isLoading: false,
+  isError: null,
+  isAuth: true,
+  isEmpty: false,
 };
 
 export default Grid;
---
+++ src/components/loading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import Box from '@mui/material/Box';
+// import Skeleton from '@mui/material/Skeleton';
+
+const Loading = () => {
+  return (
+    <div className="ag-custom-loading-cell" style={{ paddingLeft: '10px', lineHeight: '25px' }}>
+      <i className="fas fa-spinner fa-pulse" /> <span> a tak je tes</span>
+    </div>
+    // <Box className='ag-custom-loading-cell' sx={{ display: 'flex', flexDirection: 'column' }}>
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
+    //     test loading
+    // </Box>
+  );
+};
+
+export default Loading;
