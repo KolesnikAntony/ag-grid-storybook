f4dbb799acf0d480a017bfd6e1183da68d4ca035
Anton <kolesiaaa@gmail.com>
2022-04-05 17:50:18 +0300

    for stats loading3

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo, useState } from 'react';
-import { AgGridReact } from 'ag-grid-react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-enterprise';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
@@ -12,17 +12,20 @@ const Grid = (props) => {
   const containerStyle = useMemo(() => ({ width: '100%', height: '800px' }), []);
   const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
 
-  const [columnDefs] = useState([
-    { field: 'id' },
-    { field: 'athlete', width: 150 },
-    { field: 'age' },
-    { field: 'country' },
-    { field: 'year' },
-    { field: 'sport' },
-    { field: 'gold' },
-    { field: 'silver' },
-    { field: 'bronze' },
-  ]);
+  const columnDefs = [{ field: 'make'}, { field: 'model' }, { field: 'price' }];
+
+  // specify the data
+  // const rowData = [
+  //   { make: 'Toyota', model: 'Celica', price: 35000,},
+  //   { make: 'Ford', model: 'Mondeo', price: 32000 },
+  // ];
+
+  const rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000,},
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+  ];
+
+
   const defaultColDef = useMemo(() => {
     return {
       editable: true,
@@ -34,59 +37,27 @@ const Grid = (props) => {
     };
   }, []);
 
-  const loadingCellRenderer = useMemo(() => {
-    return Loading;
-  }, []);
+  const onGridReady = useCallback((params) => {}, []);
 
-  const loadingCellRendererParams = useMemo(() => {
-    return {
-      loadingMessage: 'One moment please...',
-    };
-  }, []);
 
-  const onGridReady = useCallback((params) => {
-    // fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
-    //   .then((resp) => resp.json())
-    //   .then((data) => {
-    //     // add id to data
-    //     let idSequence = 0;
-    //     data.forEach((item) => {
-    //       item.id = idSequence++;
-    //     });
-    //     const server = getFakeServer(data);
-    //     const datasource = getServerSideDatasource(server);
-    //     params.api.setServerSideDatasource(datasource);
-    //   });
+  const detailCellRenderer = useMemo(() => {
+    // return Loading;
+    return <div>test</div>;
   }, []);
-
-  const loadingCellRendererSelector = (params) => {
-    console.log(params, '---selector');
-    const useCustomRenderer = true;
-    if (useCustomRenderer) {
-      return {
-        component: Loading,
-        params: {
-          // parameters to supply to the custom loading cell renderer
-          loadingMessage: '--- CUSTOM ERROR MESSAGE ---',
-        },
-      };
-    } else {
-      // no loading cell renderer
-      return undefined;
-    }
-  };
 
   return (
     <div style={containerStyle}>
       <div style={{ height: '100%', paddingTop: '25px', boxSizing: 'border-box' }}>
         <div style={gridStyle} className="ag-theme-alpine">
           <AgGridReact
+            frameworkComponents={{
+              loading: Loading,
+            }}
             columnDefs={columnDefs}
             defaultColDef={defaultColDef}
-            loadingCellRenderer={loadingCellRenderer}
-            loadingCellRendererParams={loadingCellRendererParams}
-            // loadingCellRendererSelector={loadingCellRendererSelector}
-            // suppressLoadingOverlay={true}
+            rowData={rowData}
+            detailCellRenderer={detailCellRenderer}
+            suppressLoadingOverlay={true}
             // overlayLoadingTemplate={<Loading/>}
             cacheBlockSize={100}
             maxBlocksInCache={10}
--- src/components/loading.js
+++ src/components/loading.js
@@ -1,25 +1,11 @@
 import React from 'react';
-// import Box from '@mui/material/Box';
-// import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
+import Skeleton from '@mui/material/Skeleton';
 
 const Loading = () => {
   return (
-    <div className="ag-custom-loading-cell" style={{ paddingLeft: '10px', lineHeight: '25px' }}>
-      <i className="fas fa-spinner fa-pulse" /> <span> a tak je tes</span>
-    </div>
-    // <Box className='ag-custom-loading-cell' sx={{ display: 'flex', flexDirection: 'column' }}>
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //   {/*<Skeleton width="100%" height="40px" sx={{ mb: 2 }} />*/}
-    //     test loading
-    // </Box>
+
+      <Skeleton width={'100%'} height="40px"  />
   );
 };
 
