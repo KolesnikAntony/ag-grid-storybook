66480155c0534a25184790ce26d77946e8e00e6f
Anton <kolesiaaa@gmail.com>
2022-04-07 17:23:07 +0300

    add addons and theme

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Katarina storybook',
+    brandUrl: 'https://staging.katarina.xyz/login.php',
+    brandImage: 'https://staging.katarina.xyz/img/katarina-logo.svg',
+});
