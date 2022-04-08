2f6a0c4cabe6ea5a3308a2be154b2270ba7892c0
Anton <kolesiaaa@gmail.com>
2022-04-08 09:27:23 +0300

    help function

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,8 +1,9 @@
 import { create } from '@storybook/theming';
 
 export default create({
-    base: 'light',
+    base: 'dark',
     brandTitle: 'Katarina storybook',
     brandUrl: 'https://staging.katarina.xyz/login.php',
     brandImage: 'https://staging.katarina.xyz/img/katarina-logo.svg',
+
 });
--- src/App.js
+++ src/App.js
@@ -7,15 +7,28 @@ import 'ag-grid-enterprise';
 import Grid from './components/grid/grid';
 
 const App = () => {
+  const [state] = useState([]);
+
+  const error = null;
+
+  const getErrorText = (error) => {
+    if (error) {
+      if (error.status >= 400 && error.status <= 500) {
+        return error.message;
+      }
+      return 'Something was wrong';
+    }
+    return error;
+  };
+
   const gridProperties = {
     pagination: true,
     rowCount: 50,
     isLoading: false,
-    error: null,
+    error: getErrorText(error),
     isAuth: true,
     isEmpty: false,
   };
-  const [state] = useState([]);
 
   return <Grid {...gridProperties} state={state} />;
 };
