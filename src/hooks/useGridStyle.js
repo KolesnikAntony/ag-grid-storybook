425134a175d25e260340ec6b271da1c5b60b2bf0
Stanislav Petorv <catnislav@gmail.com>
2022-04-18 14:22:25 +0200

    -> updated a table height

--- src/hooks/useGridStyle.js
+++ src/hooks/useGridStyle.js
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 export const useGridStyle = () => {
   const containerStyle = useMemo(() => ({
     flexGrow: 1,
-    width: '100%',
+    height: '50rem',
     display: 'flex',
     flexDirection: 'column',
     borderRadius: '1.4rem',
